5a14dfb3b760396c072b7f0639b1b001e30127e6
ajay kumar <techbhoot@gmail.com>
2019-10-04 19:54:30 +0530

    cf

---
+++ bear_bigbrother.cpp
@@ -0,0 +1,25 @@
+ 
+// Made By :- Ajay Kumar
+// Date :- 01/10/2019
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+const int MOD = 1e9 + 7;
+using namespace std;
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+int32_t main() {
+      IOS
+      int a,b;
+      cin>>a>>b;
+
+      int c=0;
+      while(a<=b){
+          c++;
+          a*=3;
+          b*=2;
+      }
+      cout<<c;
+
+    return 0;
+}
+// CHECK FOR CORNER CASES LIKE 0,1 etc.
