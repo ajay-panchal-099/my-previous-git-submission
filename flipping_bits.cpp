9a5d872e3da21aedd881d7b9243d9f997aaa9f8e
ajay kumar <techbhoot@gmail.com>
2019-11-24 13:27:34 +0530

    cf

---
+++ flipping_bits.cpp
@@ -0,0 +1,23 @@
+// Made By :- Ajay Kumar
+// Date :- 01/10/2019
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+const int MOD = 1e9 + 7;
+using namespace std;
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+int32_t main() {
+      IOS
+      int t;
+      cin>>t;
+      int x=4294967295;
+
+      while(t--){
+          int y;
+          cin>>y;
+          cout<<x-y<<endl;
+      }
+
+    return 0;
+}
+// CHECK FOR CORNER CASES LIKE 0,1 etc. 
