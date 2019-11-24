a2a20ba4186d5ab2377f3e37b4bb4856eef741bc
ajay kumar <techbhoot@gmail.com>
2019-11-24 13:29:38 +0530

    cf

---
+++ cf_beta4__a.cpp
@@ -0,0 +1,23 @@
+ 
+// Made By :- Ajay Kumar
+// Date :-
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+const int MOD = 1e9 + 7;
+using namespace std;
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+int32_t main() {
+      IOS
+      int n;
+      cin>>n;
+
+      if(n%2==0 && n>2){
+          cout<<"YES"<<endl;
+      } else cout<<"NO"<<endl;
+
+
+
+    return 0;
+}
+// CHECK FOR CORNER CASES LIKE 0,1 etc.
